9931acd7160aa4e686d35cbb0b243b3fd19ece79
AdamSz98 <szekrenyes.adam98@gmail.com>
2023-07-01 17:30:17 +0200

    Create active status functionality

--- app/hooks/useActiveList.ts
+++ app/hooks/useActiveList.ts
@@ -12,4 +12,6 @@ const useActiveList = create<ActiveListStore>((set) => ({
   add: (id) => set((state) => ({ members: [...state.members, id] })),
   remove: (id) => set((state) => ({ members: state.members.filter((memberId) => memberId !== id)})),
   set: (ids) => set({ members: ids })
-}))
+}));
+
+export default useActiveList;
